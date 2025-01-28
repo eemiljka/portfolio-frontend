dc3ffb6f42896196c38d1f311373217f4068d546
eemiljka <eemiljka@metropolia.fi>
2025-01-28 19:07:58 +0200

    creating a few placeholder pages and creating a gradient background for the page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Eemil Juhani Kärkkäinen</h1>
+
+<ul>
+    <li>About me</li>
+    <li>Resume</li>
+</ul>
+
+<h2>Projects</h2>
+
+
+<h3>Device Hub</h3>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis voluptates quis nisi ipsa illo in aliquid esse enim qui optio. Id minus error sequi neque assumenda animi quas dolor praesentium.</p>
+
+<h3>JetBud</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore quia unde voluptates repellat placeat alias hic possimus corrupti, accusamus autem eius commodi soluta vel illum. Nam, commodi tenetur? Ducimus, earum?</p>
+
