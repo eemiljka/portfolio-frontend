c27c2a79936f7a6184c769ab1b28aeeed20ed59c
eemiljka <eemiljka@metropolia.fi>
2025-01-29 20:47:25 +0200

    routing nav links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -46,16 +46,16 @@
 <div class="body">
     <div class="top">
         <div class="namepart">
-        <h1 id="firstname">EEMIL</h1>
-        <h1 id="middlename">JUHANI</h1>
-        <h1 id="lastname">KÄRKKÄINEN</h1>
-        <h1 id="at">@</h1>
-        <h1 id="tilde">~/</h1>
+            <h1 id="firstname"><a href="/">EEMIL</a></h1>
+            <h1 id="middlename"><a href="/">JUHANI</a></h1>
+            <h1 id="lastname"><a href="/">KÄRKKÄINEN</a></h1>
+            <h1 id="at"><a href="/">@</a></h1>
+            <h1 id="tilde"><a href="/">~/</a></h1>
         </div>
 
 
         <ul class="top-child">
-            <li id="list-item-two"><a href="#">SKILLS & EXPERIENCE</a></li>
+            <li id="list-item-two"><a href="/skills-and-experience">SKILLS & EXPERIENCE</a></li>
             <li id="list-item-one"><a href="#">ABOUT ME</a></li>
         </ul>
 
