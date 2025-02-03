6dbfda76e04dd29d736f896f915b11d7e70c629f
eemiljka <eemiljka@metropolia.fi>
2025-02-03 18:20:04 +0200

    added background particles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 	import '../app.css';
+	import Particles from '../components/Particles.svelte';
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="global-background">
+	<Particles className="absolute inset-0 z-[-1] pointer-events-none" quantity={300} color='#ffffff' />
+</div>
+
+<div class="content">
+	{@render children()}
+</div>
