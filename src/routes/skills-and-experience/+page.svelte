5abd747f6542cb604b28af593ba1368c6a84dfb9
eemiljka <eemiljka@metropolia.fi>
2025-02-03 17:56:06 +0200

    smooth page transition from 'skills & experience' to other pages

--- src/routes/skills-and-experience/+page.svelte
+++ src/routes/skills-and-experience/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import { animate } from "motion";
 import { onMount } from "svelte";
+	import { fade } from "svelte/transition";
 
 onMount(() => {
     setTimeout(() => {
@@ -10,24 +12,39 @@ onMount(() => {
     }, 500);
 });
 
+onMount(() => {
+    animate("#tech-stack", {opacity: [0, 1], y: [-20, 0]}, {duration: 1.5})
+})
+
+let showFade = false;
+
+function toggleFade() {
+        showFade = !showFade;
+    }
+
 
 </script>
 
 <div class="body">
     <div class="top">
         <div class="namepart">
-            <h1 id="firstname"><a href="/">EEMIL</a></h1>
-        <h1 id="middlename"><a href="/">JUHANI</a></h1>
-        <h1 id="lastname"><a href="/">KÄRKKÄINEN</a></h1>
-        <h1 id="at"><a href="/">@</a></h1>
-        <h1 id="tilde"><a href="/">~/</a></h1>
+            <h1 id="firstname"><button type="button" on:click={toggleFade}><a href="/">EEMIL</a></button></h1>
+        <h1 id="middlename"><button type="button" on:click={toggleFade}><a href="/">JUHANI</a></button></h1>
+        <h1 id="lastname"><button type="button" on:click={toggleFade}><a href="/">KÄRKKÄINEN</a></button></h1>
+        <h1 id="at"><button type="button" on:click={toggleFade}><a href="/">@</a></button></h1>
+        <h1 id="tilde"><button type="button" on:click={toggleFade}><a href="/">~/</a></button></h1>
         </div>
         <ul class="top-child">
-            <li id="list-item-two"><a href="/skills-and-experience">SKILLS & EXPERIENCE</a></li>
-            <li id="list-item-one"><a href="/about">ABOUT ME</a></li>
+            <li id="list-item-two"><button type="button" on:click={toggleFade}><a href="/skills-and-experience">SKILLS & EXPERIENCE</a></button></li>
+            <li id="list-item-one"><button type="button" on:click={toggleFade}><a href="/about">ABOUT ME</a></button></li>
         </ul>
     </div>
 
+    <blockquote transition:fade={{delay: 100, duration: 100}}>
+
+    <div class="my-tech-stack">
+        <h1 id="tech-stack">MY TECH STACK</h1>
+    </div>
 
     <div class="wrapper">
         <div class="item item1">TypeScript</div>
@@ -39,6 +56,8 @@ onMount(() => {
         <div class="item item7">Next.js</div>
         <div class="item item8">SvelteKit</div>
     </div>
+
+</blockquote>
 
 
 </div>
